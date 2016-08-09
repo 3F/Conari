e5b87ec1967d6758f5d013585335b8e35fad8542
reg <entry.reg@gmail.com>
2016-08-09 14:40:41 +0300

    updated tests: IntPtrExtension & UnmanagedString

---
+++ ConariTest/Extensions/IntPtrExtensionTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using net.r_eg.Conari.Extension;
+using net.r_eg.Conari.Types;
+
+namespace net.r_eg.ConariTest.Extensions
+{
+    [TestClass]
+    public class IntPtrExtensionTest
+    {
+        [TestMethod]
+        public void GetStringLengthTest1()
+        {
+            const int BSIZE = 1;
+            string managed  = " my string 123 ";
+
+            using(var uns = new UnmanagedString(managed, UnmanagedString.SType.Ansi)) {
+                IntPtr ptr = uns;
+                Assert.AreEqual(managed.Length * BSIZE, ptr.GetStringLength(BSIZE));
+            }
+        }
+
+        [TestMethod]
+        public void GetStringLengthTest2()
+        {
+            const int BSIZE = 2;
+            string managed  = " my string 123 ";
+
+            using(var uns = new UnmanagedString(managed, UnmanagedString.SType.Unicode)) {
+                IntPtr ptr = uns;
+                Assert.AreEqual(managed.Length * BSIZE, ptr.GetStringLength(BSIZE));
+            }
+        }
+
+        [TestMethod]
+        public void GetStringLengthTest3()
+        {
+            const int BSIZE = 2;
+            string managed  = " my string 123 ";
+
+            using(var uns = new UnmanagedString(managed, UnmanagedString.SType.BSTR)) {
+                IntPtr ptr = uns;
+                Assert.AreEqual(managed.Length * BSIZE, ptr.GetStringLength(BSIZE));
+            }
+        }
+
+        [TestMethod]
+        public void GetStringBytesTest1()
+        {
+            string managed = " mystring ";
+
+            using(var uns = new UnmanagedString(managed, UnmanagedString.SType.Ansi)) {
+                IntPtr ptr = uns;
+
+                Assert.AreEqual(0, ptr.GetStringBytes(-1).Length);
+                Assert.AreEqual(0, ptr.GetStringBytes(0).Length);
+                Assert.AreEqual(5, ptr.GetStringBytes(5).Length);
+
+                var a = Encoding.UTF8.GetBytes(managed).Take(3).ToArray();
+                var b = ptr.GetStringBytes(3);
+
+                Assert.AreEqual(a[0], b[0]);
+                Assert.AreEqual(a[1], b[1]);
+                Assert.AreEqual(a[2], b[2]);
+            }
+        }
+    }
+}
