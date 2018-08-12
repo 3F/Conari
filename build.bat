c8c99033be709871670cf0c0642745e799edafaf
Denis Kuzmin [ github.com/3F ] <entry.reg@gmail.com>
2018-08-12 23:46:43 +0300

    Updated GetNuTool v1.6.2 and Added hMSBuild v1.2.2

--- build.bat
+++ build.bat
@@ -1,14 +1,15 @@
 @echo off
 
 set reltype=%1
-set _msbuild=tools/msbuild
+set _msbuild=tools/hMSBuild
+set _gnt=tools/gnt
 
 if "%reltype%"=="" (
     set reltype=Release
 )
 
 
-call gnt /p:ngconfig="packages.config" /nologo /v:m /m:4 || goto err
+call %_gnt% /p:wpath="%cd%" /p:ngconfig="packages.config" /nologo /v:m /m:4 || goto err
 call %_msbuild% -notamd64 "Conari.sln" /v:normal /l:"packages\vsSBE.CI.MSBuild\bin\CI.MSBuild.dll" /m:4 /t:Rebuild /p:Configuration=%reltype% || goto err
 
 
@@ -17,5 +18,7 @@ goto exit
 :err
 
 echo. Build failed. 1>&2
+exit /B 1
 
 :exit
+exit /B 0
